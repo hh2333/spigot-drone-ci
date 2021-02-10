1b431fdec3f91b9b3f9fb2685ab85232aca121b5
TH779 <tongsj2333@gmail.com>
2021-02-10 20:27:41 +0800

    script: No longer include upload time in build time
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ telegram_notify "开始新的构建，Spigot 版本为 ${VERSION}。"
 START=$(date +"%s")
 wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 java -jar BuildTools.jar --rev ${VERSION}
-curl -F document=@spigot-"${VERSION}".jar https://api.telegram.org/bot"${BOT_TOKEN}"/sendDocument?chat_id="${CHAT_ID}"
 END=$(date +"%s")
 DURTION=$((END - START))
+curl -F document=@spigot-"${VERSION}".jar https://api.telegram.org/bot"${BOT_TOKEN}"/sendDocument?chat_id="${CHAT_ID}"
 telegram_notify "构建完毕，此次构建耗时 $((DURTION / 60)) 分 $((DURTION % 60)) 秒。"
